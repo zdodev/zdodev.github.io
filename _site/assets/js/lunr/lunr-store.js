a01040ad9d72a38c47375a34a5bd9c7f4678f4cd
zdodev <iver0945@gmail.com>
2021-02-24 23:56:18 +0900

    📝: 테스트.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "First Post",
+        "excerpt":"Front Matter   date: 2021-02-24 title: “Jekyll 첫번째 글” categories: blog tags: jekyll  목차  toc: true  toc_sticky: true  —   ","categories": [],
+        "tags": [],
+        "url": "/first-post/",
+        "teaser": null
+      }]
